1c7fefdcac51c6da2bd83b8b255633c3e236a141
mgkdante <mgkdante@gmail.com>
2021-11-12 15:58:54 -0500

    commit

--- app/src/main/java/com/example/messengerApp/chatActivity/ChatActivity.kt
+++ app/src/main/java/com/example/messengerApp/chatActivity/ChatActivity.kt
@@ -1,19 +1,23 @@
 package com.example.messengerApp.chatActivity
 
 import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.MenuItem
-import android.view.View
 import android.widget.Button
-
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.messengerApp.R
+import android.os.Build
+import androidx.core.content.ContentProviderCompat.requireContext
+
 
 class ChatActivity : AppCompatActivity() {
     private val data = mutableListOf<MessageItemUi>()
@@ -23,6 +27,7 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var button: Button
     private val pickImage = 100
     private var imageUri: Uri? = null
+    private lateinit var bitmap: Bitmap
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,29 +50,43 @@ class ChatActivity : AppCompatActivity() {
         val recyclerView= findViewById<RecyclerView>(R.id.message)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this).apply {
-            reverseLayout = true
+           reverseLayout = true
         }
-
 
         button = findViewById(R.id.button)
         button.setOnClickListener {
-            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-            startActivityForResult(gallery, pickImage)
+            selectImage()
         }
-
-
         button2.setOnClickListener{
             insertItem()
             textView.text = ""
             recyclerView.scrollToPosition(0)
         }
     }
+    private fun selectImage(){
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(gallery, pickImage)
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == pickImage) {
             imageUri = data?.data
+            bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, imageUri!!))
+            } else {
+                MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+            }
         }
+        imageToText()
+    }
+
+    private fun imageToText(){
+        val drawable = BitmapDrawable(resources, bitmap)
+        val h = drawable.intrinsicHeight
+        val w = drawable.intrinsicWidth
+        drawable.setBounds(0, 0, w, h)
+        textView.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
     }
 
     private fun insertItem(){
